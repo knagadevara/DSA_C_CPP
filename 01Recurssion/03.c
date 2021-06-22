66703cad474c10ca60de305159337e358ab8d22a
knagadevara <vnsk.1991@gmail.com>
2021-06-22 10:52:23 -0400

    Nested Recurssion

--- 01Recurssion/03.c
+++ 01Recurssion/03.c
@@ -3,7 +3,6 @@
 void functionB(int a); // Prototyping the function
 
 // indirect recursion, passing in other function which inturn again passes the calling function.
-
 void functionA(int a)
     {
         if(a>0)
@@ -22,9 +21,17 @@ void functionB(int a)
         }
     }
 
+// Nested recursion, passing in other function which inturn again passes the calling function.
+int functionC(int a)
+    {
+        if(a>100)
+            return a-10;
+        return functionC(functionC(a+11));
+    }
 
 int main()
     {
         functionA(30);
+        functionC(100);
         return 0;
     }
